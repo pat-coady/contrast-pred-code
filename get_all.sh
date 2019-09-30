ef0d5ad6cbb980814450d0eeb5c39b6d669fe0d4
Coady <pcoady@pcoady-m01.local>
2019-09-30 14:19:38 -0400

    update get_all

--- get_all.sh
+++ get_all.sh
@@ -2,9 +2,9 @@
 DIR_NAME="replearn"
 ssh $1 "tar zcvf payload.tar.gz -C $DIR_NAME/$DIR_NAME outputs"
 scp $1:~/payload.tar.gz payload.tar.gz
-tar --keep-newer-files -xvf payload.tar.gz -C $DIR_NAME/
+tar --keep-newer-files -xvf payload.tar.gz
 rm payload.tar.gz
 ssh $1 "tar zcvf payload.tar.gz -C $DIR_NAME/$DIR_NAME logs"
 scp $1:~/payload.tar.gz payload.tar.gz
-tar --keep-newer-files -xvf payload.tar.gz -C $DIR_NAME/
+tar --keep-newer-files -xvf payload.tar.gz
 rm payload.tar.gz
